0f6f1238b7b7668e1744c1493b95e337ec1ff82c
Abhishek kumar <41527391+Invens@users.noreply.github.com>
2024-07-18 07:46:21 +0000

    backend updated

--- routes/serverRoutes.js
+++ routes/serverRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {getServersBySubscription, createServer, getServer, getAllServers, updateServer, deleteServer } = require('../controllers/serverController');
+const { getServersBySubscription, createServer, getServer, getAllServers, updateServer, deleteServer } = require('../controllers/serverController');
 const { authenticateToken } = require('../middleware/authMiddleware');
 
-router.post('/',authenticateToken, getServersBySubscription);
-router.post('/', createServer);         // Create a new server
-router.get('/:serverID', getServer);    // Get a server by ID
-router.get('/', getAllServers);         // Get all servers
-router.put('/:serverID', updateServer); // Update a server by ID
-router.delete('/:serverID', deleteServer); // Delete a server by ID
+router.get('/subscription', authenticateToken, getServersBySubscription);
+router.post('/', authenticateToken, createServer);         // Create a new server
+router.get('/:serverID', authenticateToken, getServer);    // Get a server by ID
+router.get('/', authenticateToken, getAllServers);         // Get all servers
+router.put('/:serverID', authenticateToken, updateServer); // Update a server by ID
+router.delete('/:serverID', authenticateToken, deleteServer); // Delete a server by ID
 
 module.exports = router;
